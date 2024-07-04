f3849150094a5a525bdf3e0a9c54704ce4bdcf03
Claudia Giancola <claudia.giancola@softwire.com>
2024-07-04 11:26:02 +0100

    get coordinates

--- busBoards.js
+++ busBoards.js
@@ -12,47 +12,68 @@ const require = createRequire(import.meta.url);
 
 const prompt = require("prompt-sync")();
 
-const userStop = prompt("Please insert stop code: ");
+const userPostcode = prompt("Please insert a postcode: ");
 
-function getFiveStops(body) {
-
-        //creates array of arrays containing bus line name and its relative time to station
-        const busArr = [];
-        for (let bus of body) {
-        busArr.push([bus["lineName"], bus["timeToStation"]]); 
-        }
-        
-        //console.log(busArr);
-
-        //sorts the bus array in ascending order
-        busArr.sort(function(a, b) {
-            return a[1] - b[1];
-        });
-
-        //console.log(busArr);
-
-        //prints out the next 5 buses at the chosen stop (converting seconds in minutes)
-        for (let i = 0; i < 5; i++) {
-        console.log(`The Bus number ${busArr[i][0]} is due at ${userStop} in ${Math.floor(busArr[i][1] / 60)} minutes`);
-        }
-}
-
-async function getStopInfo() {
-
-    let body;
+async function getCoordinate() {
+    let postcodeBody;
 
     try {
-        const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${userStop}/Arrivals`);
-        body = await response.json();
+        const response = await fetch(`https://api.postcodes.io/postcodes/${userPostcode}`);
+        postcodeBody = await response.json();
         if (response.status !== 200) {
             throw "Error";
-        }
-        // console.log(body);
-        getFiveStops(body);
-    } catch (error) {
-        console.log(error);
-    }
+        } 
+            const longitude = postcodeBody["result"]["longitude"];
+            const latitude = postcodeBody["result"]["latitude"];
 
+            console.log(longitude, latitude);
+
+        } catch (error) {
+            console.log(error);
+        }
 }
 
-getStopInfo();
+getCoordinate();
+
+// function getFiveStops(body) {
+
+//         //creates array of arrays containing bus line name and its relative time to station
+//         const busArr = [];
+//         for (let bus of body) {
+//         busArr.push([bus["lineName"], bus["timeToStation"]]); 
+//         }
+        
+//         //console.log(busArr);
+
+//         //sorts the bus array in ascending order
+//         busArr.sort(function(a, b) {
+//             return a[1] - b[1];
+//         });
+
+//         //console.log(busArr);
+
+//         //prints out the next 5 buses at the chosen stop (converting seconds in minutes)
+//         for (let i = 0; i < 5; i++) {
+//         console.log(`The Bus number ${busArr[i][0]} is due at ${userStop} in ${Math.floor(busArr[i][1] / 60)} minutes`);
+//         }
+// }
+
+// async function getStopInfo() {
+
+//     let body;
+
+//     try {
+//         const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${userStop}/Arrivals`);
+//         body = await response.json();
+//         if (response.status !== 200) {
+//             throw "Error";
+//         }
+//         // console.log(body);
+//         getFiveStops(body);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// }
+
+// getStopInfo();
