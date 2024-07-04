153a9920c28a7d284660f2661cae350925dd70d6
Claudia Giancola <claudia.giancola@softwire.com>
2024-07-04 15:20:08 +0100

    5 closer busses at 2 closest stops

--- busBoards.js
+++ busBoards.js
@@ -18,6 +18,9 @@ let postcodeBody;
 let coordinateBody;
 let longitude;
 let latitude;
+let stopPoint1;
+let stopPoint2;
+let result;
 
 async function getCoordinate() {
     
@@ -48,7 +51,9 @@ async function getCoordinate() {
 getCoordinate().then(() => {
     console.log(longitude);
     console.log(latitude);
-    get2StopPoints(longitude, latitude);
+    get2StopPoints(longitude, latitude).then(() => {
+    getStopInfo(stopPoint1, 0);
+    getStopInfo(stopPoint2, 1)} );
 });
 
 async function get2StopPoints(longitude, latitude) { 
@@ -68,12 +73,12 @@ async function get2StopPoints(longitude, latitude) {
             }, 1000);
           })
 
-        const result = await fetchPromise;
+        result = await fetchPromise;
 
         // console.log(result);
 
-        const stopPoint1 = result["stopPoints"][0]["naptanId"];
-        const stopPoint2 = result["stopPoints"][1]["naptanId"];
+        stopPoint1 = result["stopPoints"][0]["naptanId"];
+        stopPoint2 = result["stopPoints"][1]["naptanId"];
 
         console.log(stopPoint1, stopPoint2);
 
@@ -82,45 +87,39 @@ async function get2StopPoints(longitude, latitude) {
 }
 }
 
-// function getFiveStops(body) {
+function getFiveBuses(body, stopPoint, index) {
 
-//         //creates array of arrays containing bus line name and its relative time to station
-//         const busArr = [];
-//         for (let bus of body) {
-//         busArr.push([bus["lineName"], bus["timeToStation"]]); 
-//         }
+        //creates array of arrays containing bus line name and its relative time to station
+        const busArr = [];
+        for (let bus of body) {
+        busArr.push([bus["lineName"], bus["timeToStation"]]); 
+        }
         
-//         //console.log(busArr);
+        //sorts the bus array in ascending order
+        busArr.sort(function(a, b) {
+            return a[1] - b[1];
+        });
 
-//         //sorts the bus array in ascending order
-//         busArr.sort(function(a, b) {
-//             return a[1] - b[1];
-//         });
+        //prints out the next 5 buses at the chosen stop (converting seconds in minutes)
+        for (let i = 0; i < 5; i++) {
+        console.log(`The Bus number ${busArr[i][0]} is due at ${result["stopPoints"][index]["commonName"]} (${result["stopPoints"][index]["indicator"]}) in ${Math.floor(busArr[i][1] / 60)} minutes`);
+        }
+}
 
-//         //console.log(busArr);
+async function getStopInfo(stopPoint, index) {
 
-//         //prints out the next 5 buses at the chosen stop (converting seconds in minutes)
-//         for (let i = 0; i < 5; i++) {
-//         console.log(`The Bus number ${busArr[i][0]} is due at ${userStop} in ${Math.floor(busArr[i][1] / 60)} minutes`);
-//         }
-// }
+    let body;
 
-// async function getStopInfo() {
+    try {
+        const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPoint}/Arrivals`);
+        body = await response.json();
+        if (response.status !== 200) {
+            throw "Error";
+        }
+        // console.log(body);
+        getFiveBuses(body, stopPoint, index);
+    } catch (error) {
+        console.log(error);
+    }
 
-//     let body;
-
-//     try {
-//         const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${userStop}/Arrivals`);
-//         body = await response.json();
-//         if (response.status !== 200) {
-//             throw "Error";
-//         }
-//         // console.log(body);
-//         getFiveStops(body);
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-// }
-
-// getStopInfo();
+}
